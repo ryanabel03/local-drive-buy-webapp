bf53923e4ecd31ba53cf451386c5ee593e2b358d
Ryan Abel <abelr@mail.gvsu.edu>
2013-02-04 16:38:10 -0500

    Initialized empty rails app and added a few gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocalDriveBuyWebapp::Application.config.secret_token = 'd40058c9b5ba4634fd24603342b6358fc6f0726be1b4652777d403ca4e0643e534dc1818f13d572da759d2d8bd6746fa30a7c676b3f691c321d1b5f944521a09'
