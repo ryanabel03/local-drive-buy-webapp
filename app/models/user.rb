72b0f095e82bf22f65d40b9af1a7f61df1cf9d09
Ryan Abel <abelr@mail.gvsu.edu>
2013-04-16 13:21:20 -0400

    Added avatar support for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :first_name, :last_name, :phone, :state, :zip, :address_one,
-                  :address_two, :city
+                  :address_two, :city, :avatar
 
+  has_attached_file :avatar, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: "images/:style/missing.png"
   has_many :listings
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130409185530) do
+ActiveRecord::Schema.define(:version => 20130416170751) do
 
   create_table "images", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -64,6 +64,10 @@ ActiveRecord::Schema.define(:version => 20130409185530) do
     t.string   "state"
     t.string   "city"
     t.integer  "zip"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
