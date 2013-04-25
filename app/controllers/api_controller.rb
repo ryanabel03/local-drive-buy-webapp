90eda58d7e9631990c4cda1a6599a5a917061674
Ryan Abel <abelr@mail.gvsu.edu>
2013-04-24 20:48:28 -0400

    Add user check

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,18 +3,19 @@ class ApiController < ApplicationController
     listings = Listing.all
     listings.each do |listing|
       user = User.find_by_id(listing.user_id)
-      puts user.avatar.inspect
-      listing["user_info"] = 
-        {
-          phone: user.phone,
-          business_name: user.business_name,
-          city: user.city,
-          state: user.state,
-          zip: user.zip,
-          address_one: user.address_one,
-          address_two: user.address_two,
-          pic_url: user.avatar
-      }
+      if user
+        listing["user_info"] = 
+          {
+            phone: user.phone,
+            business_name: user.business_name,
+            city: user.city,
+            state: user.state,
+            zip: user.zip,
+            address_one: user.address_one,
+            address_two: user.address_two,
+            pic_url: user.avatar
+        }
+      end
     end
 
     render json: listings
